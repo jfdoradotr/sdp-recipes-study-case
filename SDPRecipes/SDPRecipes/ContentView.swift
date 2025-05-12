1fd9687ab243d2bedf75165909de79ccb524e516
Juan Dorado <jfdoradotr@icloud.com>
2025-05-12 09:49:35 -0600

    Create the project

---
+++ SDPRecipes/SDPRecipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDPRecipes
+//
+//  Created by Juan Francisco Dorado Torres on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ SDPRecipes/SDPRecipes/SDPRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDPRecipesApp.swift
+//  SDPRecipes
+//
+//  Created by Juan Francisco Dorado Torres on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SDPRecipesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
