bc95eaafd219bf2c92bf23d5d05005a0e5510674
Juan Dorado <jfdoradotr@icloud.com>
2025-05-14 19:16:50 -0600

    Refactor details view

--- SDPRecipes/SDPRecipes/Views/RecipeDetailsView.swift
+++ SDPRecipes/SDPRecipes/Views/RecipeDetailsView.swift
@@ -12,80 +12,102 @@ struct RecipeDetailsView: View {
 
   var body: some View {
     Form {
-      Section {
-        ZStack {
-          AsyncImage(url: recipe.image) { phase in
-            switch phase {
-            case .empty:
-              ProgressView()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            case .success(let image):
-              image
-                .resizable()
-                .scaledToFill()
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .frame(height: 250)
-            case .failure(let error):
-              Image(systemName: "photo.badge.exclamationmark.fill")
-              .font(.largeTitle)
-              .frame(maxWidth: .infinity)
-            @unknown default:
-              Image(systemName: "photo.badge.exclamationmark.fill")
-              .font(.largeTitle)
-              .frame(maxWidth: .infinity)            }
-          }
-        }
-
-        .frame(height: 250)
-      }
-      .listRowBackground(Color.clear)
-      .listRowInsets(EdgeInsets())
-
-      Section {
-        VStack(alignment: .leading, spacing: 12) {
-          Text(recipe.name)
-            .font(.title2)
-            .fontWeight(.bold)
-          VStack(alignment: .leading, spacing: 8) {
-            Text("⭐️ \(recipe.rating.formatted())")
-            Text("🍽️ \(recipe.servings) people")
-            Text("⏲️ \(recipe.prepTimeMinutes) min")
-            Text("⏲️ \(recipe.caloriesPerServing) cal/portion")
-          }
-        }
-      } header: {
-        Text("Basic Info")
-      }
-
-      Section {
-        ForEach(recipe.ingredients, id: \.self) { ingredient in
-          HStack(alignment: .top) {
-            Text("•")
-              .font(.headline)
-            Text(ingredient)
-          }
-          .listRowSeparator(.hidden)
-        }
-      } header: {
-        Text("Ingredients")
-      }
-
-      Section {
-        ForEach(recipe.instructions, id: \.self) { instruction in
-          HStack(alignment: .top) {
-            Text("•")
-              .font(.headline)
-            Text(instruction)
-          }
-          .listRowSeparator(.hidden)
-        }
-      } header: {
-        Text("Instructions")
-      }
+      coverSectionView
+      basicInfoSectionView
+      bulletedItemsSectionView(
+        items: recipe.ingredients,
+        header: "Ingredients"
+      )
+      bulletedItemsSectionView(
+        items: recipe.instructions,
+        header: "Instructions"
+      )
     }
     .navigationBarTitleDisplayMode(.inline)
   }
 }
+
+// MARK: - Views
+
+private extension RecipeDetailsView {
+  var coverSectionView: some View {
+    Section {
+      ZStack {
+        AsyncImage(url: recipe.image) { phase in
+          switch phase {
+          case .empty:
+            ProgressView()
+              .frame(maxWidth: .infinity, maxHeight: .infinity)
+          case .success(let image):
+            image
+              .resizable()
+              .scaledToFill()
+              .clipShape(RoundedRectangle(cornerRadius: 10))
+              .frame(height: 250)
+          case .failure:
+            Image(systemName: "photo.badge.exclamationmark.fill")
+              .font(.largeTitle)
+              .frame(maxWidth: .infinity)
+          @unknown default:
+            Image(systemName: "photo.badge.exclamationmark.fill")
+              .font(.largeTitle)
+              .frame(maxWidth: .infinity)
+          }
+        }
+      }
+      .frame(height: 250)
+    }
+    .listRowBackground(Color.clear)
+    .listRowInsets(EdgeInsets())
+  }
+
+  var basicInfoSectionView: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(recipe.name)
+          .font(.title2)
+          .fontWeight(.bold)
+        VStack(alignment: .leading, spacing: 8) {
+          Text("⭐️ \(recipe.rating.formatted())")
+          Text("🍽️ \(recipe.servings) people")
+          Text("⏲️ \(recipe.prepTimeMinutes) min")
+          Text("🔥 \(recipe.caloriesPerServing) cal/portion")
+        }
+      }
+    } header: {
+      Text("Basic Info")
+    }
+  }
+
+  func bulletedItemsSectionView(items: [String], header: String) -> some View {
+    Section {
+      BulletedItemsView(items: items)
+        .listRowSeparator(.hidden)
+    } header: {
+      Text(header)
+    }
+  }
+}
+
+// MARK: - BulletedItemsView
+
+private extension RecipeDetailsView {
+  struct BulletedItemsView: View {
+    let items: [String]
+
+    var body: some View {
+      ForEach(items, id: \.self) { item in
+        HStack(alignment: .top) {
+          Text("•")
+            .font(.headline)
+          Text(item)
+        }
+      }
+    }
+  }
+}
+
+// MARK: - Preview
 
 #Preview {
   RecipeDetailsView(recipe: .preview)
